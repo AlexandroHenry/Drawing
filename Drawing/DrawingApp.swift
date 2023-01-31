748af3ba6b3eb0a35fec2e7d7b832e802c075d78
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-31 12:14:21 +0900

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Seungchul Ha on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
